d1b42ff931f05cad71a0982ab015ad8e9f059c11
Ridwan <ricklick262@gmail.com>
2021-08-30 21:44:13 +0100

    ...

--- src/routes/message.js
+++ src/routes/message.js
@@ -3,50 +3,37 @@ import { Router } from 'express';
  
 const router = Router();
 
-router.get('/', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
+router.get('/', async (req, res) => {
+    const messages = await req.context.models.Message.find();
+    return res.send(messages);
   });
-
-router.post('/', (req, res) => {
-    const id = uuidv4();
-    const message = {
-        id,
-        text: req.body.text,
-        userId: req.context.me.id,
-    };
-
-    req.context.models.messages[id] = message;
+   
+  router.get('/:messageId', async (req, res) => {
+    const message = await req.context.models.Message.findById(
+      req.params.messageId,
+    );
     return res.send(message);
-});
-
-
-router.get('/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-
-router.put('/:messageId', (req, res) => {
-    const { 
-        [req.params.messageId]: message, 
-        ...otherMessages
-        } = req.context.models.messages;
-
-    message.text = req.body.text
-
-    req.context.models.messages = {...req.context.models.messages, [req.params.messageId]: message }
-
-    return res.send(req.context.models.messages)
-})
-
-router.delete('/:messageId', (req, res) => {
-    const {
-        [req.params.messageId]: message,
-        ...otherMessages
-    } = req.context.models.messages;
-
-    req.context.models.messages = otherMessages;
-
+  });
+   
+  router.post('/', async (req, res) => {
+    const message = await req.context.models.Message.create({
+      text: req.body.text,
+      user: req.context.me.id,
+    });
+   
     return res.send(message);
-});
+  });
+   
+  router.delete('/:messageId', async (req, res) => {
+    const message = await req.context.models.Message.findById(
+      req.params.messageId,
+    );
+   
+    if (message) {
+      await message.remove();
+    }
+   
+    return res.send(message);
+  });
 
 export default router
